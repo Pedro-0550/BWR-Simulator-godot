423954d88d83b7ce84a33815071f744c01b75c92
Pedro-0550 <pedrocamedeiros13@gmail.com>
2023-05-26 17:39:35 -0300

    commit

--- Assets/Scripts/Physics.gd
+++ Assets/Scripts/Physics.gd
@@ -1,14 +1,17 @@
+class_name Physics
+extends Node
+
 var Size = {
-    X = 58,
-    Y = 58,
-    Z = 20
+	X = 5,
+	Y = 5,
+	Z = 10
 }
 
-var Sources = {}
+var Sources = [5*5*10, 5*5*10, 5*5*10, 5*5*10, 5*5*10]
 var Cells = {}
 var Rods = {}
 
-var MaxNeutrons = 10^6
+var MaxNeutrons = 10^5
 var StartupNeutrons = 100
 
 var NeutronTransferIterations = 5
@@ -21,75 +24,82 @@ var DecayRate = 0.05
 var BurnupRate = 0.005
 
 func _ready():
-    for X in Size.X:
-        Rods[X] = {}
-        for Y in Size.Y:
-            Rods[Y] = 0
-            
-            for Z in Size.Z:
-                Cells[X][Y][Z] = {
-                    Neutrons = StartupNeutrons if Sources.has(X*Y*Z) else 0,
+	for X in range(1, Size.X):
+		Rods[X] = {}
+		Cells[X] = {}
+		
+		for Y in range(1, Size.Y):
+			Rods[X][Y] = 0
+			Cells[X][Y] = {}
 
-                    Xe135Concentration = 10,
-                    I135Concentration = 10,
+			for Z in range(1, Size.Z):
+				Cells[X][Y][Z] = {
+					Neutrons = StartupNeutrons if Sources.has(X*Y*Z) else 0,
 
-                    RemainingU235 = 1000
-                }
-                
+					Xe135Concentration = 10,
+					I135Concentration = 10,
+
+					RemainingU235 = 1000
+				}
+				
 
 #TODO: Implement fuel burnup
 func _physics_process(_delta):
-    TransferNeutrons()
+	TransferNeutrons()
 
-    for X in Size.X:
-        for Y in Size.Y:
-            for Z in Size.Z:
-                Cells[X][Y][Z].Neutrons *= randi_range(1,3)
-                Cells[X][Y][Z].I135Concentration += ((Cells[X][Y][Z].Neutrons/MaxNeutrons) * ByproductRate) - Cells[X][Y][Z].I135Concentration * DecayRate
-                Cells[X][Y][Z].Xe135Concentration += Cells[X][Y][Z].I135Concentration * DecayRate
+	for X in range(1, Size.X):
+		for Y in range(1, Size.Y):
+			for Z in range(1, Size.Z):
+				Cells[X][Y][Z].Neutrons = Cells[X][Y][Z].Neutrons * 3
+				Cells[X][Y][Z].Neutrons += StartupNeutrons if Sources.has(X*Y*Z) else 0
+				
+				Cells[X][Y][Z].I135Concentration += ((Cells[X][Y][Z].Neutrons/MaxNeutrons) * ByproductRate) - Cells[X][Y][Z].I135Concentration * DecayRate
+				Cells[X][Y][Z].Xe135Concentration += Cells[X][Y][Z].I135Concentration * DecayRate
+
+				print(Cells[X][Y][Z].Neutrons/MaxNeutrons)
 
 #TODO: Implement xenon and iodine impact on absorption
 func TransferNeutrons():
-    for Iteration in NeutronTransferIterations:
-        for X in Size.X:
-            for Y in Size.Y:
-                for Z in Size.Z:
-                    var Neighbors = GetNeighbors(X, Y, Z)
+	for Iteration in NeutronTransferIterations:
+		for X in range(1, Size.X):
+			for Y in range(1, Size.Y):
+				for Z in range(1, Size.Z):
+					var Neighbors = GetNeighbors(X, Y, Z)
 
-                    var Cell = Cells[X][Y][Z]
-                    var ZPosition = (Z / Size.Z) * 100
-                    var TransferedNeutrons = Cell.Neutrons * 0.8
+					var Cell = Cells[X][Y][Z]
+					var ZPosition = (Z / Size.Z) * 100
+					var TransferedNeutrons = Cell.Neutrons * 0.8
 
-                    var CRCoefficient = 0
+					var CRCoefficient = 0
 
-                    if abs(ZPosition - Rods[X][Y]) < Size.Z:
-                        CRCoefficient = (ZPosition - Rods[X][Y]) / Size.Z
+					if abs(ZPosition - Rods[X][Y]) < Size.Z:
+						CRCoefficient = (ZPosition - Rods[X][Y]) / Size.Z
 
-                    Cells[X][Y][Z].Neutrons -= TransferedNeutrons
-                    Cells[X][Y][Z].Neutrons = clamp(Cells[X][Y][Z].Neutrons, 0, MaxNeutrons^2)
+					Cells[X][Y][Z].Neutrons -= TransferedNeutrons
+					Cells[X][Y][Z].Neutrons = clamp(Cells[X][Y][Z].Neutrons, 0, MaxNeutrons^2)
 
-                    for Neighbor in Neighbors:
-                        if not Cells[Neighbor.X] and not Cells[Neighbor.X][Neighbor.Y] and not Cells[Neighbor.X][Neighbor.Y][Neighbor.Z]:
-                            pass
+					for Neighbor in Neighbors:
+						if not Cells.has(Neighbor.X) or not Cells[Neighbor.X].has(Neighbor.Y) or not Cells[Neighbor.X][Neighbor.Y].has(Neighbor.Z):
+							continue
 
-                        if Neighbor.X == 0 and Neighbor.Y == 0 and abs(Neighbor.Z) == 1:
-                            Cells[Neighbor.X][Neighbor.Y][Neighbor.Z].Neutrons += TransferedNeutrons / 26
-                        else:
-                            Cells[Neighbor.X][Neighbor.Y][Neighbor.Z].Neutrons += (TransferedNeutrons / 26) * CRCoefficient
-                        
+						if Neighbor.X - X == 0 and Neighbor.Y - Y == 0 and abs(Neighbor.Z - Z) == 1:
+							Cells[Neighbor.X][Neighbor.Y][Neighbor.Z].Neutrons += TransferedNeutrons / 26
+						else:
+							Cells[Neighbor.X][Neighbor.Y][Neighbor.Z].Neutrons += (TransferedNeutrons / 26) * CRCoefficient
+						
 
-                    
+					
 
 func GetNeighbors(X, Y, Z):
-    var Neighbors = []
+	var Neighbors = []
 
-    for I in [-1, 0, 1]:
-        for J in [-1, 0, 1]:
-            for K in [-1, 0, 1]:
-                if I == 0 or J == 0 or K == 0:
-                    pass
-                Neighbors.append({ X = X + I, Y = Y + J, Z = Z + K })
+	for I in [-1, 0, 1]:
+		for J in [-1, 0, 1]:
+			for K in [-1, 0, 1]:
+				if I == 0 and J == 0 and K == 0:
+					continue
+				Neighbors.append({ X = X + I, Y = Y + J, Z = Z + K })
 
-    return Neighbors
-                
-                
+	return Neighbors
+				
+				
